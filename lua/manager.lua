9c51f289d78c48b78e8e3d8f4acd0f873f844070
Dovie Weinstock <DovieWeinstock@gmail.com>
2023-12-18 18:22:52 -0500

    changes from dovies-g14-popos on Mon Dec 18 06:22:52 PM EST 2023

--- lua/manager.lua
+++ lua/manager.lua
@@ -299,12 +299,12 @@ require('lazy').setup({
 		opts = {}
 	},
 
-	{
-		'goolord/alpha-nvim',
-		config = function()
-			require 'alpha'.setup(require 'alpha.themes.dashboard'.config)
-		end
-	},
+	-- {
+	-- 	'goolord/alpha-nvim',
+	-- 	config = function()
+	-- 		require 'alpha'.setup(require 'alpha.themes.dashboard'.config)
+	-- 	end
+	-- },
 
 	-- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
 	--       These are some example plugins that I've included in the kickstart repository.
