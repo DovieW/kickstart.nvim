76ca40b515e6e5db58a54af76cb05ea0aec579f3
dovw <dovw@bhphoto.com>
2024-07-24 10:10:07 -0400

    changes from BH6559 on Wed Jul 24 10:10:07 EDT 2024

--- lua/manager.lua
+++ lua/manager.lua
@@ -420,6 +420,8 @@ require('lazy').setup({
 		config = true,
 	},
 
+	{ 'github/copilot.vim' },
+
 	-- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
 	--       These are some example plugins that I've included in the kickstart repository.
 	--       Uncomment any of the lines below to enable them.
